8d7499d7ca3e33fe09d734c57534200c3fa52992
hprasad <hrishikeshprasadc@gmail.com>
2024-01-15 22:36:31 +0530

    Netflix design done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Banner from './components/Banner/Banner'
+import NavBar from './components/NavBar/NavBar'
+import RowPost from './components/RowPost/RowPost'
+
+function App() {
+  
+
+  return (
+    <div>
+     <NavBar/>
+     <Banner/>
+     <RowPost/>
+    </div>
+  )
+}
+
+export default App
